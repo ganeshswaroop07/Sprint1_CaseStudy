18e881c0b13968b25f84be1c2fb645981515b042
Shivani Tijare <shivanitijare19@gmail.com>
2021-09-02 18:06:09 +0530

    commit_123

---
+++ CaseStudy/MedicalDonationSystem_BAL/Validations/NGO_Validations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalDonationSystem_BAL.Validations
+{
+    class NGO_Validations
+    {
+    }
+}
---
+++ CaseStudy/MedicalDonationSystem_CAL/MedicalDonationSystem_UI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MedicalDonationSystem_CAL
+{
+    class MedicalDonationSystem_UI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
